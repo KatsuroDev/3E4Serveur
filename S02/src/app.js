810691ddb814ed83e30c8c06c43b8df222232eb3
KatsuroDev <katsuro.userg@gmail.com>
2021-09-02 16:43:02 -0400

    add different routes

--- S02/src/app.js
+++ S02/src/app.js
@@ -15,14 +15,47 @@ app.get("/date", (req, res) => {
     res.send(`${dayjs()}`)
 });
 
-app.get("/somme", (req, res) => {
+//maths/somme
+//maths/difference
+//maths/produit
+//maths/quotient
+//maths/reste
+
+app.get("/maths/:operation", (req, res) => {
     //console.log(req.query);
 
+    const operation = req.params.operation;
+    console.log(operation);
+
+        
     const a = parseInt(req.query.a, 10);
     const b = parseInt(req.query.b, 10);
+    
+    let result = 0;
+    switch(operation) {
+        case 'somme':
+            result = a + b;
+            break;
+        case 'difference':
+            result = a - b;
+            break;
+        case 'produit':
+            result = a * b;
+            break;
+        case 'quotient':
+            result = a / b;
+            break;
+        case 'reste':
+            result = a % b;
+            break;
+        default:
+            res.status(400);
+            return res.send('Opération non reconnue');
+    }
+
     res.status(200);
     res.set("Content-Type", "text/html");
-    res.send(`<strong>${a + b}</strong>`);
+    res.send(`<strong>${result}</strong>`);
 });
 
 export default app;
