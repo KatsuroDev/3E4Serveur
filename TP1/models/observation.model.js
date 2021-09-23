a933bd017b0bbde8873049685b4cb2dfe8dc2bf2
KatsuroDev <katsuro.userg@gmail.com>
2021-09-23 16:39:57 -0400

    add complete observation model

--- TP1/models/observation.model.js
+++ TP1/models/observation.model.js
@@ -7,7 +7,22 @@ const observationSchema = mongoose.Schema({
             lat: {type:Number, required:true},
             lon: {type:Number, required:true}
         }
-    }
+    },
+    temperature:Number,
+    pressure:Number,
+    humidity: {type:Number, min:0, max:1},
+    feelslike:Number,
+    uvIndex: {type:Number, min:0, max:11},
+    wind: {
+        speed:Number,
+        degree: {type:Number, min:0, max:360}
+    },
+    clouds: {
+        cloudcover: {type:Number, min:0, max:1}
+    },
+    observationDate: {type:Date, default: Date.now},
+    hexMatrix: [String]
+
 }, {
     collection:"observations",
     strict:"throw"
