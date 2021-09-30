9994b5876d8c892e13737c1de6d07ea30d3a57d4
KatsuroDev <katsuro.userg@gmail.com>
2021-09-30 16:49:20 -0400

    add base client side

---
+++ S06/js/index.js
@@ -0,0 +1,33 @@
+const SERVICE_URL = "https://api.andromia.science/planets/";
+
+$(document).ready(() => {
+    getPlanets();
+});
+
+
+async function getPlanets() {
+    try {
+        const response = await axios.get(SERVICE_URL);
+        if(response.status === 200) {
+            const planets = response.data;
+            planets.forEach(p => {
+                $('#planets').append(displayPlanet(p));
+            });
+        } else {
+            console.log(response);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+function displayPlanet(planet) {
+    let planetHtml = '<div class="card col-2">';
+
+    planetHtml += `<a href="details.html"><img src="${planet.icon}"}></a>`
+    planetHtml += `<a href="details.html"><h5 class="nom-planet">${planet.name}</h5></a>`
+
+    planetHtml += '</div>';
+
+    return planetHtml;
+}
